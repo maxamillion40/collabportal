09efa0ea81a7ee19593d27e6819d4151a7878301
Christian Danscheid <Christian_D_97@gmx.de>
2013-08-19 17:29:18 +0200

    Footer verschoenert und Credits gesetzt

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 ﻿<div id="footer">
-	<p>Scratch ist ein Projekt der Lifelong-Kindergarten-Gruppe am Media-Lab des MIT<br />
-	ScratchCollabs ist ein Projekt von webdesigner97</p>
+	<p>Scratch ist ein Projekt der Lifelong-Kindergarten-Gruppe am Media-Lab des MIT</p>
+	<p>ScratchCollabs ist ein Projekt von <a href="http://scratch.mit.edu/users/webdesigner97/">webdesigner97</a> und <a href="http://scratch.mit.edu/users/Lirex/">Lirex</a></p>
 </div>
