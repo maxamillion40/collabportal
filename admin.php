7c1a5ceca03729c7f1c1599007aff8bfdc4b6700
Christian Danscheid <Christian_D_97@gmx.de>
2013-09-14 15:55:00 +0200

    admin.php angelegt

---
+++ admin.php
@@ -0,0 +1,51 @@
+<?php
+	session_start();
+	require_once("includes/func.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Über &raquo; ScratchCollabs in DACH</title>
+		<!-- Meta -->
+		<meta charset="utf-8" />
+		<meta name="description" content="Das CollabPortal ermöglicht es dir, auf einfache Weise Scratch Collabs zu erstellen, zu verwalten und zu veranstalten." />
+		<meta name="keywords" content="scratch,collabs,dach,deutsch" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="styles/main.css" />
+		<link rel="stylesheet" href="styles/cp.css" />
+		<link rel="stylesheet" href="styles/about.css" />
+		<!-- Scripts -->
+		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
+		<script src="scripts/init.js"></script>
+	</head>
+	<body>
+		<div id="pagewrapper">
+			<!-- This is the blue box on the top of the site -->
+				<?php
+					include_once("includes/topnav.php");
+				?>
+			<!-- Main Content -->	
+			<div class="container" id="content">
+				<div class="cols clearfix" style="top: -10px; padding-left: 10px; padding-right: 10px;">
+					<!-- Über -->	
+						<article class="box ">
+							<div class="box-head">
+								<h4>Collabverwaltung</h4>
+							</div>
+							<div class="box-content">
+								<div class="inner">
+									
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+		</div>
+		<!-- Footer -->
+		<?php
+			include_once("includes/footer.php");
+		?>
+		</div>
+	</body>
+</html>
