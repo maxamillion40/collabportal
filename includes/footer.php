95076421a0b08d1baddd2bbfd36aff6d4e6d7beb
Christian Danscheid <Christian_D_97@gmx.de>
2013-09-16 19:05:32 +0200

    Nicht geschlossenes <a> gefixt

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 ﻿<div id="footer">
-	<p><ul id="footer-menu"><li></li><li><a>Kontakt<a></li><li><a>Über ScratchCollabs</li><li><a>Nutzungsbedingungen</a></li></ul></p>
+	<p><ul id="footer-menu"><li></li><li><a>Kontakt</a></li><li><a>Über ScratchCollabs</a></li><li><a>Nutzungsbedingungen</a></li></ul></p>
 	<p>Scratch ist ein Projekt der Lifelong-Kindergarten-Gruppe am Media-Lab des MIT</p>
 	<p>ScratchCollabs ist ein Projekt von <a href="http://scratch.mit.edu/users/webdesigner97/">webdesigner97</a> und <a href="http://scratch.mit.edu/users/Lirex/">Lirex</a></p>
 </div>
