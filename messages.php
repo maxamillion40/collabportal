08fcf922f03ffa086c20a4ab6914d6f514a46e6a
Christian Danscheid <Christian_D_97@gmx.de>
2013-09-18 20:33:38 +0200

    Auflistung der Nachrichten

--- messages.php
+++ messages.php
@@ -5,8 +5,8 @@
 		header("Location: index.php");
 	}
 	mysql_auto_connect();
-	$unread = count(mysql_get("SELECT * FROM `messages` WHERE `to`='".$_SESSION["user"]."'"));
-	$msg = mysql_get("SELECT * FROM `messages` WHERE `to`='".$_SESSION["user"]."'");
+	$unread = count(mysql_get("SELECT * FROM `messages` WHERE `to`='".$_SESSION["user"]."' AND `read`='0'"));
+	$msg = mysql_get("SELECT * FROM `messages` WHERE `to`='".$_SESSION["user"]."' ORDER BY `read` ASC, `date` DESC");
 	mysql_close();
 ?>
 <!DOCTYPE html>
@@ -20,6 +20,7 @@
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="styles/main.css" />
 		<link rel="stylesheet" href="styles/cp.css" />
+		<link rel="stylesheet" href="styles/messages.css" />
 		<!-- Scripts -->
 		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
 		<script src="scripts/init.js"></script>
@@ -38,7 +39,41 @@
 					</div>
 					<div class="box-content">
 						<div class="inner">
-							<p>Du hast <?php echo $unread; ?> ungelesene Nachrichten!</p>
+							<p>
+								<?php
+									if($unread == 1)	{
+										echo "Du hast 1 ungelesene Nachricht.";
+									}
+									else	{
+										echo "Du hast ".$unread." ungelesene Nachrichten.";
+									}
+								?>
+							</p>
+							<?php
+								if(count($msg) > 0)	{
+									echo "<table id='msg-table'>";
+									echo "<colgroup>";
+									echo "<col class='sendtime' />";
+									echo "<col class='sender' />";
+									echo "<col class='regard' />";
+									echo "<col class='read' />";
+									echo "</colgroup>";
+									foreach($msg as $m)	{
+										echo "<tr>";
+											echo "<td class=''>".date("d.m.Y H:i",$m["date"])."</td>";
+											echo "<td>".$m["sender"]."</td>";
+											echo "<td>".$m["regard"]."</td>";
+											if($m["read"] == "0")	{
+												echo "<td>Ungelesen</td>";
+											}
+											else	{
+												echo "<td>Gelesen</td>";
+											}
+										echo "</tr>";
+									}
+									echo "</table>";
+								}
+							?>
 						</div>
 					</div>
 				</article>
