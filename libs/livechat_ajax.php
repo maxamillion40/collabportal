3ab609b5d1c4ca27a766e7093e6cb902d41461d1
Christian Danscheid <Christian_D_97@gmx.de>
2013-09-20 17:28:59 +0200

    Ueberhaengender Chattext Fix, hoffentlich

--- libs/livechat_ajax.php
+++ libs/livechat_ajax.php
@@ -20,10 +20,10 @@
 		foreach($messages as $msg)	{
 			if(!$msg["censored"])	{
 				echo "<div class='msg msgid-".$msg["id"]."'>";
-				echo "<div class='msghead'>";
+				echo "<div class='msg-head'>";
 					echo "<button onClick='javascript: navigate(\"action.php?censore&collab=".$collab."&msg=".$msg["id"]."\");' class='censore-button' title='Zensieren'>X</button> ".$msg["absender"]." am ".$tage[date("l",$msg["timestamp"])].", ".date("d.m.Y h:i",$msg["timestamp"]);
 					echo "</div>";
-				echo "<div class='msgbody'>";
+				echo "<div class='msg-body'>";
 					echo $msg["message"];
 				echo "</div>";
 				echo "</div>";
