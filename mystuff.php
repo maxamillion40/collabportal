84c86342afeb20a219f8af931a6b9da19535cabe
AlZiWi <alex-zimmer@online.de>
2013-10-13 14:26:46 +0200

    Ausgebaut

--- mystuff.php
+++ mystuff.php
@@ -5,6 +5,7 @@
 	if(is_loggedin()) {
 		$user = $_SESSION['user'];
 		$mycollabs = mysql_get("SELECT * FROM collabs WHERE `owner`='$user'");
+		$collabmember = mysql_get("SELECT * FROM collabs");
 	}
 	else {
 		die(header("Location: index.php?error=nologin"));
@@ -38,7 +39,7 @@
 			<div class="container" id="content">
 				<!-- div für obere boxen -->
 				<div class="cols clearfix" style="top: -10px; padding-left: 10px; padding-right: 10px;">
-					<!-- Hilfe -->	
+					<!-- Liste -->	
 						<article class="box ">
 							<div class="box-head">
 								<h3>Meine Collabs</h3><span id='newcollab'><a href='new.php'><button>+ Neues Collab</button></a></span>
@@ -47,26 +48,41 @@
 								<div class="inner">
 									<?php
 										print_array($mycollabs);
-										echo "<ul id='msu'>";
+										echo "<ul id='msu'><hr/>";
 										if(count($mycollabs) > 0)	{
 											//Collabliste
 											foreach($mycollabs as $collab)	{
-												echo "<li>";
+												echo "<button class='button grey' onClick=\"navigate('admin.php?id=".$collab["id"]."');\">Verwaltung</button><li>";
 												echo "<a href='collab.php?id=".$collab["id"]."'><img src='logos/".$collab["logo"]."' width='144' height='108' class='image' alt='".$collab["name"]."' /></a>";
 												echo "<table class='stats'>";
-													echo "<tr><td><b>Name:</b></td><td>".$collab["name"]."</td></tr>";
-													echo "<tr><td><b>Status:</b></td><td>".$collab["status"]."</td></tr>";
-													echo "<tr><td><b>Mitglieder:</b></td><td><ul class='cmembers'>";
-													$collab["mitglieder"] = unserialize($collab["mitglieder"]);
-													foreach($collab["mitglieder"]["people"] as $member) {
-														echo "<li>".$member."</li>";
-													}
-												echo "</ul></td></tr></table></li><hr/>";
+													echo "<tr><th>Name:</th><td>".$collab["name"]."</td></tr>";
+													echo "<tr><th>Status:</th><td>".$collab["status"]."</td></tr>";
+													echo "<tr><th>Rang:</th><th>Eigner</th></tr>";
+												echo "</td></tr></table></li><hr/>";
 											}
 										}
 										else	{
 											//Keine Collabs
 											echo "Du hast noch keine Collabs erstellt. <a href='new.php'>Erstelle eins!</a>";
+										}
+										echo "<span class='strong'><hr/></span>";
+											$count = 0;
+											foreach($collabmember as $collab)	{
+												$collab["mitglieder"] = unserialize($collab["mitglieder"]);
+												if(in_array($_SESSION["user"],$collab["mitglieder"]["people"]))	{
+													echo "<button class='button grey' onClick=\"navigate('action.php?leave&red&id=".$collab["id"]."','Willst du wirklich aus dem Collab &bdquo;".$collab["name"]."&ldquo; austreten?')\">Austreten</button><li>";
+													echo "<a href='collab.php?id=".$collab["id"]."'><img src='logos/".$collab["logo"]."' width='144' height='108' class='image' alt='".$collab["name"]."' /></a>";
+													echo "<table class='stats'>";
+														echo "<tr><th>Name:</th><td>".$collab["name"]."</td></tr>";
+														echo "<tr><th>Status:</th><td>".$collab["status"]."</td></tr>";
+														echo "<tr><th>Rang:</th><td>Mitglied</td></tr>";
+														echo "<tr><th>Gründer:</th><td>".$collab["owner"]."</td></tr>";
+													echo "</td></tr></table></li><hr/>";
+													$count++;
+												}
+											}
+										if($count == 0) {
+											echo "Du hast noch an keinem Collab teilgenommen. <a href='./'>Nimm an einem teil!</a><hr/>";
 										}
 										echo "</ul>";
 									?>
