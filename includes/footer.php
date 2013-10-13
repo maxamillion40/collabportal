b39273f7ff5d6b33042ea7df15431c2809ff4abb
AlZiWi <alex-zimmer@online.de>
2013-10-13 14:31:14 +0200

    Menupunkt hinzugeuegt

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 ﻿<div id="footer">
-	<p><ul id="footer-menu"><li></li><li><a>Kontakt</a></li><li><a>Über ScratchCollabs</a></li><li><a>Nutzungsbedingungen</a></li></ul></p>
+	<p><ul id="footer-menu"><li><a href="contact.php">Kontakt</a></li><li><a href="about.php">Über ScratchCollabs</a></li><li><a href="team.php">Das Team</a></li><li><a href="termsofuse.php">Nutzungsbedingungen</a></li></ul></p>
 	<p>Scratch ist ein Projekt der Lifelong-Kindergarten-Gruppe am Media-Lab des MIT</p>
 	<p>ScratchCollabs ist ein Projekt von <a href="http://scratch.mit.edu/users/webdesigner97/">webdesigner97</a> und <a href="http://scratch.mit.edu/users/Lirex/">Lirex</a></p>
 </div>
