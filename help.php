8549c2575b3edbdbd8f3f9c1d91dc6fe1ba6af71
AlZiWi <alex-zimmer@online.de>
2013-10-25 13:21:35 +0200

    Vorbereitungen fuer FAQ

--- help.php
+++ help.php
@@ -20,6 +20,16 @@
 		<script src="scripts/init.js"></script>
 	</head>
 	<body>
+		<script>
+			function scrollup () {
+				if (location.hash)
+					window.scrollBy(0, -40);
+			}
+			function lscrollup () {
+				window.setTimeout('window.scrollBy(0, -40)',10);
+			}
+			window.onload = scrollup;
+		</script>
 		<div id="pagewrapper">
 			<!-- This is the blue box on the top of the site -->
 				<?php
@@ -35,12 +45,71 @@
 							</div>
 							<div class="box-content" style="height: auto;">
 								<div class="inner">
-									<h4>Kurzhilfe</h4>
+									<h3>Kurzhilfe</h3>
 										<p>Hier wird bald eine Kurzanleitung zur Benutzung des CollabPortals folgen.</p>
-									<h4>Wiki-Artikel</h4>
+									<h3>Wiki-Artikel</h3>
 										<p>Das CollabPortal hat auch einen Artikel im deutschsprachigen Scratch-Wiki: <a href="http://scratch-dach.info/wiki/CollabPortal" >http://scratch-dach.info/wiki/CollabPortal</a>.</p>
-									<h4>FAQ</h4>
-										<p>Hier werden häufig gestellte Fragen beantwortet werden, zur Zeit wurden aber noch nicht besonders viele gestellt.</p>
+									<hr/>
+									<h3>FAQ</h3>
+										<p>Du hast eine Frage? Stelle sie <a href="./">hier</a> oder <a href="contact.php">kontaktiere uns</a>!<br/>Klicke auf die Überschriften, um einen Permalink zu erhalten.</p>
+										<!-- Hier kommt später mal PHP-Code hin, damit Fragen und Antworten vom Admin über die Datenbank hinzugefügt werden können. -->
+									<div id="questions">
+										<div id="1"><a href="help.php#1" onclick="lscrollup()"><h4>Was ist das Collabportal?</h4></a>
+											<p>Siehe: <a href="about.php">Über ScratchCollabs</a>.</p></div>
+										<div id="2"><a href="help.php#2" onclick="lscrollup()"><h4>Was ist Scratch?</h4></a>
+											<p>Siehe: <a href="http://scratch.mit.edu/about" >Über Scratch</a>.</p></div>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+											b<br>
+									</div>
 								</div>
 							</div>
 						</article>
