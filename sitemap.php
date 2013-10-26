b318bb0362621c11202b9f31184d52d771f10228
webdesigner97 <Christian_D_97@gmx.de>
2013-10-26 11:18:01 +0200

    Sitemap

---
+++ sitemap.php
@@ -0,0 +1,12 @@
+﻿<?php
+	header("Content-type: application/xml");
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	<url>
+		<loc>http://<?php echo $_SERVER["SERVER_NAME"]; ?>/index.php</loc>
+		<lastmod><?php echo date("Y-m-d",filemtime("index.php")); ?></lastmod>
+		<changefreq>weekly</changefreq>
+		<priority>1</priority>
+	</url>
+</urlset> 
