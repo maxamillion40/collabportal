13c86db2317e189e7218cbe75da0401dadb4a7af
webdesigner97 <Christian_D_97@gmx.de>
2013-10-26 11:27:40 +0200

    Automatische Generierung der Sitemap mittels includes/sitemap-data.php

---
+++ includes/sitemap-data.php
@@ -0,0 +1,16 @@
+﻿<?php
+	$map = array();
+	//
+	$map[] = array(
+		"file" => "index.php",
+		"priority" => "0.9"
+	);
+	$map[] = array(
+		"file" => "about.php",
+		"priority" => "0.6",
+	);
+	$map[] = array(
+		"file" => "help.php",
+		"priority" => "0.4",
+	);
+?>
--- sitemap.php
+++ sitemap.php
@@ -1,12 +1,19 @@
 ﻿<?php
 	header("Content-type: application/xml");
+	include("includes/sitemap-data.php");
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<url>
-		<loc>http://<?php echo $_SERVER["SERVER_NAME"]; ?>/index.php</loc>
-		<lastmod><?php echo date("Y-m-d",filemtime("index.php")); ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>1</priority>
-	</url>
+	<?php
+		foreach($map as $url)	{
+			if(isset($url["file"]) and isset($url["priority"]))	{
+				echo "<url>";
+				echo "<loc>http://" . $_SERVER["SERVER_NAME"] . "/" . $url["file"] . "</loc>";
+				echo "<lastmod>" . date("Y-m-d",filemtime($url["file"])) . "</lastmod>";
+				echo "<changefreq>weekly</changefreq>";
+				echo "<priority>" . $url["priority"] . "</priority>";
+				echo "</url>";
+			}
+		}
+	?>
 </urlset> 
