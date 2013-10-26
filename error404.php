74d63eeed1f4f82f9420ba9f7dc72080bd9a3b99
webdesigner97 <Christian_D_97@gmx.de>
2013-10-26 17:35:33 +0200

    Neuer Entwurf

--- error404.php
+++ error404.php
@@ -19,6 +19,7 @@
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="styles/main.css" />
 		<link rel="stylesheet" href="styles/cp.css" />
+		<link rel="stylesheet" href="styles/error404.css" />
 		<!-- Scripts -->
 		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
 		<script src="scripts/init.js"></script>
@@ -33,12 +34,19 @@
 			<div class="container" id="content">
 				<article class="box">
 					<div class="box-head">
-						<h4>Houston, wir haben ein Problem</h4>
+						<h4>Route 404 - Falsch abgebogen?</h4>
 					</div>
-					<div class="box-content">
+					<div class="box-content" id="box-404">
 						<div class="inner">
-							<div id="error404">
-								<img src="img/cat.png" alt="Scratch Cat" />
+							<p class="centered">Wir haben unser Bestes gegeben, aber diese Seite konnten wir einfach nicht finden...</p>
+							<p class="centered">Hast du dich vielleicht vertippt oder hast du einen sehr alten Link benutzt?</p>
+							<div id="box404">
+								<img src="img/script404.png" width="305" height="129" border="0" usemap="#map" />
+								<map name="map">
+									<area shape="rect" coords="14,39,146,63" href="index.php" alt="Zurück zu Startseite" />
+									<area shape="rect" coords="14,63,184,88" href="javascript: history.back();" alt="Zurück zur vorherigen Seite" />
+									<area shape="rect" coords="0,100,77,130" href="http://scratch.mit.edu" alt="Scratch on!" />
+								</map>
 							</div>
 						</div>
 					</div>
