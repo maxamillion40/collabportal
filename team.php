f045b2ee4ad4dc52e773c247dfd0efb44f0c1b56
AlZiWi <alex-zimmer@online.de>
2013-11-01 15:05:33 +0100

    Angelegt

---
+++ team.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	require_once("includes/func.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Das Team &raquo; ScratchCollabs in DACH</title>
+		<!-- Meta -->
+		<meta charset="utf-8" />
+		<meta name="description" content="Das CollabPortal ermöglicht es dir, auf einfache Weise Scratch Collabs zu erstellen, zu verwalten und zu veranstalten." />
+		<meta name="keywords" content="scratch,collabs,dach,deutsch" />
+		<meta name="robots" content="noindex,nofollow" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="styles/main.css" />
+		<link rel="stylesheet" href="styles/cp.css" />
+		<link rel="stylesheet" href="styles/team.css" />
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.ico" />
+		<!-- Scripts -->
+		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
+		<script src="scripts/init.js"></script>
+	</head>
+	<body>
+		<div id="pagewrapper">
+			<!-- This is the blue box on the top of the site -->
+				<?php
+					include_once("includes/topnav.php");
+				?>
+			<!-- Main Content -->	
+			<div class="container" id="content">
+				<!-- div für obere boxen -->
+				<div class="cols clearfix" style="top: -10px; padding-left: 10px; padding-right: 10px;">
+					<!-- Liste -->	
+						<article class="box ">
+							<div class="box-head">
+								<h3>Das Team</h3>
+							</div>
+							<div class="box-content">
+								<div class="inner">
+									<h3>Administratoren</h3>
+										<img src="http://cdn.scratch.mit.edu/get_image/user/909333_75x75.png?v=1367988496.15" width="75px" /><div class="desc"><h4>webdesigner97</h4>
+										webdesigner97 ist Gründer und Hauptadministrator des Collabportals. [...]</div>
+										<img src="http://cdn.scratch.mit.edu/get_image/user/1900672_75x75.png?v=1380467242.62" width="75px" /><div class="desc"><h4>Lirex</h4>
+										Lirex stieß am 8. August 2013 dazu, nachdem er die Entwicklung des Collabportals seit den Anfängen Ende 2012 mitverfolgt hatte.
+										Er ist für diverse Texte und Grafiken, die FAQ und für das Design zuständig. Zudem moderiert Lirex das Collabportal und ist Ansprechpartner bei Problemen.</div>
+									<h3>Tester</h3>
+										<!-- Hier kommen besondere Tester hin -->
+									<h3>Betatester</h3>
+										<ul class="desc">
+											<li>Ominöse Person</li>
+										</ul>
+									<h3>Danke außerdem an:</h3>
+										<ul class="desc">
+											<li>LiFaytheGoblin (Logo, Ideengeberin)</li>
+											<li>Das Scratch Team (Scratch)</li>
+										</ul>
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+		</div>
+		<!-- Footer -->
+		<?php
+			include_once("includes/footer.php");
+		?>
+		</div>
+	</body>
+</html>
