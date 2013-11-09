9caf1887abfa9ba4667066e1a17592c3b3af3af0
webdesigner97 <Christian_D_97@gmx.de>
2013-11-09 17:32:00 +0100

    Anpassungen an Server

--- libs/setpass.php
+++ libs/setpass.php
@@ -1,13 +1,13 @@
 ﻿<?php
 	//Get data
 	$who = $_SESSION["user"];
-	$realpw = mysql_get("SELECT `pass` FROM `users` WHERE `name`='$who'")[0]["pass"];
+	$realpw = mysql_get("SELECT `pass` FROM `users` WHERE `name`='$who'");
 	$old = md5($_POST["old"]);
 	$new = md5($_POST["new"]);
 	$new2 = md5($_POST["new-2"]);
 	if($old != "" and $new != "" and $new2 != "")	{
 		if($new == $new2)	{
-			if($old == $realpw)	{
+			if($old == $realpw[0]["pass"])	{
 				mysql_query("UPDATE `users` SET `pass`='$new' WHERE `name`='$who'");
 				header("Location: settings.php?result=editok");
 			}
