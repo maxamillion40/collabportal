0a00db542b138d708735fe647b0494a2fc94fdce
webdesigner97 <Christian_D_97@gmx.de>
2013-11-15 13:14:29 +0100

    Flackern entfernt

--- scripts/livechat.php
+++ scripts/livechat.php
@@ -16,50 +16,55 @@
 		echo "var member = false \n";
 	}
 ?>
+var reload = true;
 var window_focus = true;
 var title = $("title").html();
 var cid = $(document).getUrlParam("id");
 var msgInterval;
+var msgInterval2;
 var msg = new Array;
 	msg["msgCount"] = Number.POSITIVE_INFINITY;
 //
 function getAllMessages(id)	{
 	if(member == true)	{
-		$.ajax({
-			url: "libs/livechat_ajax.php?id=" + id,
-			dataType: "json",
-			type: "GET",
-			success: function(data)	{
-				// Reset
-				window.clearInterval(msgInterval);
-				// Message count
-				var oldcount = msg["msgCount"];
-				var newcount = data["msgCount"];
-				// Inform user about new messages
-				msg = data;
-				if((newcount > oldcount && $("#livechat").visible(true) == false) || window_focus == false)	{
-					msgInterval = window.setInterval(function()	{
-						$("title").html(newcount - oldcount + " neue Nachrichten");
-						window.setTimeout(function()	{
+		if(reload == true)	{
+			$.ajax({
+				url: "libs/livechat_ajax.php?id=" + id,
+				dataType: "json",
+				type: "GET",
+				success: function(data)	{
+					// Reset
+					window.clearInterval(msgInterval);
+					// Message count
+					var oldcount = msg["msgCount"];
+					var newcount = data["msgCount"];
+					// Inform user about new messages
+					msg = data;
+					if((newcount > oldcount && $("#livechat").visible(true) == false) || (window_focus == false && newcount > oldcount))	{
+						reload = false;
+						msgInterval = window.setInterval(function()	{
+							$("title").html("Neue Nachricht!");
+						},2000);
+						msgInterval2 = window.setInterval(function()	{
 							$("title").html(title);
-						},1000);
-					},2000);
+						},3000);
+					}
+					// Insert messages
+					$("#livechat").html("");
+					var max = msg["msgCount"];
+					for(i=0;i<max;i++)	{
+						var output;
+						var m = msg["msgList"][i];
+						var output = "<div class='msg msg-" + m["id"] + "'><div class='msg-head'>" + m["absender"] + " am " + m["timestamp"] + "</div><div class='msg-body'>" + m["message"] + "</div></div>";
+						$("#livechat").append(output);
+					}
+					scratchblocks2.parse("pre.blocks");
+				},
+				error: function(jqXHR, textStatus, errorThrown)	{
+					$("#livechat").html("Fehler beim Laden der Nachrichten: " + errorThrown);
 				}
-				// Insert messages
-				$("#livechat").html("");
-				var max = msg["msgCount"];
-				for(i=0;i<max;i++)	{
-					var output;
-					var m = msg["msgList"][i];
-					var output = "<div class='msg msg-" + m["id"] + "'><div class='msg-head'>" + m["absender"] + " am " + m["timestamp"] + "</div><div class='msg-body'>" + m["message"] + "</div></div>";
-					$("#livechat").append(output);
-				}
-				scratchblocks2.parse("pre.blocks");
-			},
-			error: function(jqXHR, textStatus, errorThrown)	{
-				//
-			}
-		});
+			});
+		}
 	}
 	else	{
 		$("#livechat").html("Nachrichten sind nur für Mitglieder sichtbar.");
@@ -72,8 +77,11 @@ $(document).ready(function()	{
 });
 
 window.setInterval(function()	{
-	if($("#livechat").visible(true))	{
+	if($("#livechat").visible(true) && window_focus == true)	{
 		window.clearInterval(msgInterval);
+		window.clearInterval(msgInterval2);
+		reload = true;
+		$("title").html(title);
 	}
 },500);
 window.setInterval(function()	{
