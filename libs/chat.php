62b2b3db44c0f680e362c9dcdb1a05fa008400b5
webdesigner97 <Christian_D_97@gmx.de>
2013-11-16 16:10:55 +0100

    Umwandlung :) zu Smilies

--- libs/chat.php
+++ libs/chat.php
@@ -4,6 +4,13 @@
 	$msg	= mysql_real_escape_string($_POST["msg"]);
 	$von	= $_SESSION["user"];
 	$time	= time();
+	//Prepare message
+	$msg = trim($msg);
+	$msg = str_replace(":)","<img src=\"http://cbeta.scratchcollabs.pf-control.de/scripts/tinymce/plugins/emoticons/img/smiley-smile.gif\" alt=\":)\" />",$msg);
+	$msg = str_replace(":(","<img src=\"http://cbeta.scratchcollabs.pf-control.de/scripts/tinymce/plugins/emoticons/img/smiley-cry.gif\" alt=\":(\" />", $msg);
+	$msg = str_replace(";)", "<img src=\"http://cbeta.scratchcollabs.pf-control.de/scripts/tinymce/plugins/emoticons/img/smiley-wink.gif\" alt=\";)\" />",$msg);
+	$msg = str_replace(":P", "<img src=\"http://cbeta.scratchcollabs.pf-control.de/scripts/tinymce/plugins/emoticons/img/smiley-tongue-out.gif\" alt=\":P\" />",$msg);
+	$msg = str_replace(":O","<img src=\"http://cbeta.scratchcollabs.pf-control.de/scripts/tinymce/plugins/emoticons/img/smiley-surprised.gif\" alt=\":O\" />",$msg);
 	//Check data
 	if($msg == "")	{
 		die(header("Location: collab.php?id=$collab&error=notext"));
@@ -12,7 +19,7 @@
 		die(header("Location: index.php?error=noid"));
 	}
 	//Insert message into db
-	mysql_query("INSERT INTO `collabmessages`(`timestamp`,`absender`,`collab`,`message`) VALUES('$time','$von','$collab','$msg')");
+	mysql_query("INSERT INTO `collabmessages`(`timestamp`,`absender`,`collab`,`message`) VALUES('$time','$von','$collab','$msg')") or die(mysql_error());
 	//Back to collab page
 	header("Location: collab.php?id=$collab&result=msgok#livechat");
 ?>
