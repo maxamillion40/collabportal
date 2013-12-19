a750c03c995138f7fb7ae7384c9cdd363e938a4a
webdesigner97 <Christian_D_97@gmx.de>
2013-12-19 14:04:59 +0100

    uebersetzung

---
+++ includes/locale.php
@@ -0,0 +1,11 @@
+﻿<?php
+	$_LOCALE = array();
+	$files = scandir("locale");
+	unset($files[0]);
+	unset($files[1]);
+	
+	foreach($files as $l)	{
+		$filedata = parse_ini_file("locale/" . $l, true);
+		$_LOCALE[$filedata["meta"]["langcode"]] = $filedata["translations"];
+	}
+?>
