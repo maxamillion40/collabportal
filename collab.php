b7822be500322d6027518f0448dcc465113da6e9
webdesigner97 <Christian_D_97@gmx.de>
2014-02-28 13:01:42 +0100

    jetzt aber, fixes #35

--- collab.php
+++ collab.php
@@ -39,7 +39,7 @@
 		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
 		<script src="scripts/jquery/jquery.getUrlParam.js"></script>
 		<script src="scripts/tinymce/tinymce.min.js"></script>
-		<script src="scripts/jquery/jquery.tinymce.min.js"></script>
+		<script src="scripts/tinymce/jquery.tinymce.min.js"></script>
 		<script src="scripts/scratchblocks2.js"></script>
 		<script src="scripts/init.js"></script>
 		<script src="scripts/infinitescroll.js"></script>
