5f55cc057c5e7943dc91d096fb7f41b4fb55f6d2
webdesigner97 <Christian_D_97@gmx.de>
2014-02-28 12:59:14 +0100

    collab.php ausgemistet, fixes 35

--- collab.php
+++ collab.php
@@ -37,11 +37,9 @@
 		<link rel="shortcut icon" href="favicon.ico" />
 		<!-- Scripts -->
 		<script src="scripts/jquery/jquery-1.10.2.min.js"></script>
-		<script src="scripts/jqueryui/jquery-ui-1.10.3.custom.min.js"></script>
 		<script src="scripts/jquery/jquery.getUrlParam.js"></script>
-		<script src="scripts/jquery.visible.min.js"></script>
-		<script src="scripts/jquery.visibility.js"></script>
 		<script src="scripts/tinymce/tinymce.min.js"></script>
+		<script src="scripts/jquery/jquery.tinymce.min.js"></script>
 		<script src="scripts/scratchblocks2.js"></script>
 		<script src="scripts/init.js"></script>
 		<script src="scripts/infinitescroll.js"></script>
