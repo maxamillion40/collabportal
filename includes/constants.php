a7e08729143e0f68506d1b71818cb6663435fb34
webdesigner97 <Christian_D_97@gmx.de>
2014-05-12 17:32:27 +0200

    add github link

--- includes/constants.php
+++ includes/constants.php
@@ -3,5 +3,6 @@
 	define("CP_DESCRIPTION", "Bla");
 	define("CP_KEYWORDS", "scratch, collabs");
 	define("CP_TITLE_SPLITTER","&raquo;");
+	define("CP_GITHUB_REPO", "https://github.com/webdesigner97/collabportal");
 	define("BR", "\r\n");
 ?>
