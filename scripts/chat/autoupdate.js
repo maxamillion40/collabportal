4025e13ac568d543f97a3adaedd83d1019dfedd4
webdesigner97 <Christian_D_97@gmx.de>
2014-07-02 11:25:09 +0200

    verzweifelte fix versuche

--- scripts/chat/autoupdate.js
+++ scripts/chat/autoupdate.js
@@ -1,8 +1,11 @@
-﻿function countMessages()	{
+﻿var loading = true;
+
+function countMessages()	{
 	var elem = $("#livechat div.msg").attr("class");
 	if(elem)	{
 		elem = elem.split(" ")[1];
 		elem = Number(elem.match(/\d/g).join(""));
+		console.log("Latest message: " + elem)
 		return elem;
 	}
 	else	{
@@ -24,11 +27,12 @@ function getMessagesOnServer()	{
 			loading = false;
 		}
 	});
+	console.log("Latest message on server: " + r)
 	return r;
 }
 
 function hasNewMessages()	{
-	if(getMessagesOnServer() > countMessages())	{
+	if(Number(getMessagesOnServer()) > Number(countMessages()))	{
 		return true;
 	}
 	else	{
@@ -37,23 +41,29 @@ function hasNewMessages()	{
 }
 
 function loadNewMessages(startAt)	{
+	console.log("Starting download of new messages");
+	console.log("Calling " + "libs/loadCollabMessages.ajax.php?lastID=" + startAt.toString() + "&cid=" + $(document).getUrlParam("id") + "&method=initial");
 	$.ajax({
-		url: "libs/loadCollabMessages.ajax.php?lastID=" + startAt + "&cid=" + $(document).getUrlParam("id") + "&method=initial",
+		url: "libs/loadCollabMessages.ajax.php?lastID=" + startAt.toString() + "&cid=" + $(document).getUrlParam("id") + "&method=initial",
 		dataType: "text",
 		type: "GET",
 		success: function(data)	{
 			if(data.length > 5)	{
+				console.log("Received new messages");
 				$("#livechat").prepend(data);
 			}
 		},
 		error: function(jqXHR, textStatus, errorThrown)	{
+			console.log("Error");
 			loading = false;
 		}
 	});
 }
 
 window.setInterval(function()	{
+	console.log("Checking for new messages");
 	if(hasNewMessages())	{
+		console.log("New messages found");
 		loadNewMessages(countMessages());
 	}
 }, 2000);
