8f0e9f0d7337619a3dc247faa3a6077a1f216ad2
webdesigner97 <Christian_D_97@gmx.de>
2014-08-28 19:30:31 +0200

    fix loading of latest 10 messages

--- scripts/chat/chat.js
+++ scripts/chat/chat.js
@@ -1,13 +1,24 @@
 ﻿"use strict";
 
 $(document).ready(function()	{
-	// Get the first 10 messages and output them
-	getMessagesFromInterval(1, 10, function(data)	{
-		var i;
-		for(i = 0; i < data.count; i++)	{
-			var msg = data.list[i];
-			// Prepend to #livechat
-			$("#livechat").prepend('<div class="msg msg-' + msg.internalID + '"><div class="msg-head"><span class="msg-name">' + msg.absender + '</span>, ' + msg.timestamp + '</div><div class="msg-body">' + msg.message + '</div>');
-		}
+	// Get the internalID of the latest message
+	$.ajax({
+		url: "ajax/getLatestMessageID.ajax.php",
+		dataType: "json",
+		type: "POST",
+		data: {
+			cid: $(document).getUrlParam("id")
+		},
+		success: function(data)	{
+			// Get the first 10 messages and output them
+			getMessagesFromInterval(data.internalID - 9, data.internalID, function(data)	{
+				var i;
+				for(i = 0; i < data.count; i++)	{
+					var msg = data.list[i];
+					// Prepend to #livechat
+					$("#livechat").prepend('<div class="msg msg-' + msg.internalID + '"><div class="msg-head"><span class="msg-name">' + msg.absender + '</span>, ' + msg.timestamp + '</div><div class="msg-body">' + msg.message + '</div>');
+				}
+			});
+		},
 	});
 });
